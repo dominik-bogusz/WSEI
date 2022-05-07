61b0b5aed3eb5f76de61cb720873a2b2422c7df8
dominik.bogusz <dominik.bogusz@PK5-07>
2022-05-07 14:14:37 +0200

    Adding folder lab_05

---
+++ lab_05/lab_05/ObservableList1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_05
+{
+    class ObservableList1
+    {
+        public double Length { get; set; }
+
+
+
+        public void Add()
+        {
+
+        }
+        public void Get()
+        {
+
+        }
+        public void Set()
+        {
+
+        }
+        public void RemoveAt()
+        {
+
+        }
+    }
+}
---
+++ lab_05/lab_05/ObservableList3.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_05
+{
+    class ObservableList3
+    {
+        public delegate void Added();
+        public delegate void Updated();
+        public delegate void Removed();
+    }
+}
